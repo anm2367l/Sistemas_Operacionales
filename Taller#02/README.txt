1ff2a04524bc34b4e9d4b0c7fcd19a43109d67a8
anm2367l <88902130+anm2367l@users.noreply.github.com>
2023-03-28 19:50:39 -0500

    Taller#02

---
+++ Taller#02/README.txt
@@ -0,0 +1,2 @@
+Para que los script funcionen, hace falta pasarles el nombre de la carpeta a crear, por ejm:
+bash p4.sh taller02
---
+++ Taller#02/p4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source p1.sh $1
+echo "Creacion de directorios completada"
+source p2.sh $1
+echo "Suma de vectores completada"
+source p3.sh $1
+echo "Compresion completada"
